9c29d16699515b462d60871a3873fbd81ae9ad39
Kevin Cruz <kevfaridc@gmail.com>
2023-04-19 14:38:53 -0500

    chore: config project

---
+++ src/theme.ts
@@ -0,0 +1,25 @@
+import { extendTheme, withDefaultColorScheme } from '@chakra-ui/react';
+
+const theme = extendTheme(
+  {
+    config: {
+      initialColorMode: 'dark',
+      useSystemColorMode: false,
+    },
+    styles: {
+      global: {
+        'body, html': {
+          padding: 0,
+          margin: 0,
+        },
+        '*': {
+          boxSizing: 'border-box',
+          scrollBehavior: 'smooth',
+        },
+      },
+    },
+  },
+  withDefaultColorScheme({ colorScheme: 'blue' })
+);
+
+export default theme;
